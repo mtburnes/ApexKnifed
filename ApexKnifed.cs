1636a351a7a882ca342e84fe094a826101d5ff32
Michael Burnes <me@mtburnes.dev>
2020-06-23 00:06:12 -0500

    Init ApexKnifed.cs

---
+++ ApexKnifed.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Timers;
+
+using PRoCon.Core;
+using PRoCon.Core.Plugin;
+using PRoCon.Core.Plugin.Commands;
+using PRoCon.Core.Players;
+using PRoCon.Core.Players.Items;
+using PRoCon.Core.Battlemap;
+using PRoCon.Core.Maps;
+using PRoCon.Core.HttpServer;
+
+namespace ProConEvents {
+    public class ApexKnifed : ProConPluginAPI, IPRoConPluginInterface {
+        private const String PluginVersion = "1.0";
+
+        
+    }
+}
