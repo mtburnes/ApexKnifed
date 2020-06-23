dbc885d665764368246a3e39191c2ccbed2b6168
Michael Burnes <me@mtburnes.dev>
2020-06-23 00:16:47 -0500

    Boilerplate

--- ApexKnifed.cs
+++ ApexKnifed.cs
@@ -21,6 +21,52 @@ namespace ProConEvents {
     public class ApexKnifed : ProConPluginAPI, IPRoConPluginInterface {
         private const String PluginVersion = "1.0";
 
-        
+        public enum GameVersion {
+            BF4
+        };
+
+        //bools
+        private bool m_isPluginEnabled;
+
+        public ApexKnifed() {
+            
+
+            this.m_isPluginEnabled = false;
+        }
+
+        public string GetPluginName() {
+            return "ApexKnifed";
+        }
+
+        public string GetPluginVersion() {
+            return PluginVersion;
+        }
+
+        public string GetPluginAuthor() {
+            return "mtburnes";
+        }
+
+        public string GetPluginWebsite() {
+            return "https://github.com/mtburnes/ApexKnifed";
+        }
+
+        public string GetPluginDescription() {
+            return @"
+<h2>Description</h2>
+    <p>Creates custom 'player got knifed' messages.</p>
+		";
+        }
+
+        public void OnPluginEnable() {
+            this.ExecuteCommand("procon.protected.pluginconsole.write", "^bApexKnifed ^2Enabled!");
+
+            this.m_isPluginEnabled = true;
+        }
+
+                public void OnPluginDisable() {
+            this.ExecuteCommand("procon.protected.pluginconsole.write", "^bApexKnifed ^1Disabled!" );
+
+            this.m_isPluginEnabled = false;
+        }
     }
 }
