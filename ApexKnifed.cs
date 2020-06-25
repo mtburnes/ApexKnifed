7b57960e117322c39b010eece0eec0a711aff0c3
Michael Burnes <me@mtburnes.dev>
2020-06-25 08:13:39 -0500

    Current bit of what script I have. (not working)

--- ApexKnifed.cs
+++ ApexKnifed.cs
@@ -63,10 +63,79 @@ namespace ProConEvents {
             this.m_isPluginEnabled = true;
         }
 
-                public void OnPluginDisable() {
+        public void OnPluginDisable() {
             this.ExecuteCommand("procon.protected.pluginconsole.write", "^bApexKnifed ^1Disabled!" );
 
             this.m_isPluginEnabled = false;
+        }
+
+        public void OnPluginLoaded(String strHostName, String strPort, String strPRoConVersion) {
+            Log.Debug("Entering OnPluginLoaded", 7);
+            try {
+                //Set the server IP
+                _serverInfo.ServerIP = strHostName + ":" + strPort;
+                //Register all events
+                RegisterEvents(GetType().Name,
+                    "OnListPlayers",
+                    "OnPlayerKilled",
+                    );
+            } catch (Exception e) {
+                Log.Exception("FATAL ERROR on plugin load.", e);
+            }
+            Log.Debug("Exiting OnPluginLoaded", 7);
+
+
+        public List<CPluginVariable> GetDisplayPluginVariables() {
+
+            List<CPluginVariable> lstReturn = new List<CPluginVariable>();
+			lstReturn.Add(new CPluginVariable("ApexKnifed|Enabled", typeof(bool), ApexKnifedEnable));
+
+            return lstReturn;
+        }
+
+        public void SetPluginVariable(string strVariable, string strValue) {
+			if (Regex.Match(strVariable, @"Enabled").Success)
+            {
+				bool tmpApexKnifedEnable = false;
+                Boolean.TryParse(strValue, out tmpApexKnifedEnable);
+				ApexKnifedEnable = tmpApexKnifedEnable;
+            }
+			else if (Regex.Match(strVariable, @"Delay").Success)
+            {
+                int tmpTime = 15;
+                int.TryParse(strValue, out tmpTime);
+                ApexKnifedDelay = tmpTime;
+            }
+        }
+
+        public override void OnPlayerKilled(Kill kill) {
+            Log.Debug("Entering OnPlayerKilled", 7);
+            try {
+                //If the plugin is not enabled and running just return
+                if (!_pluginEnabled || !_threadsReady || !_firstPlayerListComplete) {
+                    return;
+            }
+            AdKatsSubscribedPlayer killer;
+            AdKatsSubscribedPlayer victim;
+            if (kill.Killer != null && !String.IsNullOrEmpty(kill.Killer.SoldierName)) {
+                    if (!_playerDictionary.TryGetValue(kill.Killer.SoldierName, out killer)) {
+                        Log.Error("Unable to fetch killer " + kill.Killer.SoldierName + " on kill.");
+                        return;
+                    }
+                } else {
+                    return;
+            }
+
+            if (kill.Victim != null && !String.IsNullOrEmpty(kill.Victim.SoldierName)) {
+                    if (!_playerDictionary.TryGetValue(kill.Victim.SoldierName, out victim)) {
+                        Log.Error("Unable to fetch victim " + kill.Victim.SoldierName + " on kill.");
+                        return;
+                    }
+                } else {
+                    return;
+            }
+            if (kill.)
+
         }
     }
 }
